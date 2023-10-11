99050d2fb1cba61b3261f9b2cd8b7b13ab08199f
Peyton-Smith05 <89094185+Peyton-Smith05@users.noreply.github.com>
2023-10-11 09:58:14 -0500

    Java Documentation Initial

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+/**
+ * The main entry point for the application.
+ * <p>
+ * This method launches the JavaFX application by invoking the {@link GUI#launch(Class, String[])}
+ * method with the specified GUI class and command-line arguments.
+ * </p>
+ * <p>
+ * To run this application, use the following command:
+ * <pre>
+ *     javac --module-path /lib --add-modules javafx.controls
+ * </pre>
+ * <pre>
+ *     javafx.fxml -d bin src/App.java
+ * </pre>
+ * </p>
+ *
+ * @param args The command-line arguments passed to the application.
+ */
 public class App {
-     // run with javac --module-path /lib --add-modules javafx.controls,javafx.fxml -d bin src/App.java
     public static void main(String[] args) {
         GUI.launch(GUI.class, args);
     }
